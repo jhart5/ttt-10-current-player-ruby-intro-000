1afd9d168dca32ad359fdc18aa5030b611b31789
josh hart <joshua.hart@protonmail.com>
2019-09-15 20:44:24 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
-  turn_num = 1
+  turn_num = 0
   board.each do |space|
     if (space == "X")||(space == "O")
       turn_num += 1
